104865da91755ba7e7b9c1455ee6a02b0b7400fe
sudha-achanta <110560014+sudha-achanta@users.noreply.github.com>
2023-02-26 15:33:47 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,17 @@ provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
+
+resource "aws_s3_bucket" "java-deploy" {
+  bucket = "tf-java-bucket"
+
+  tags = {
+    Name        = "java-tf"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "my-s3-acl" {
+  bucket = aws_s3_bucket.java-deploy.id
+  acl    = "public"
+}
